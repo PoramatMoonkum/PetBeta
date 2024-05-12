7d7728f9633b213cafae8636767f8855d5abf3ab
Poramat Moonkum <porramatmoon@gmail.com>
2024-05-12 21:30:54 +0700

    profile but google not sussucs

--- lib/view/profile/profile_view.dart
+++ lib/view/profile/profile_view.dart
@@ -1,189 +1,126 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:pettakecare/common/color_extension.dart';
-import 'package:pettakecare/common_widget/round_button.dart';
-import 'package:pettakecare/common_widget/round_textfield.dart';
 
 class ProfileView extends StatefulWidget {
-  const ProfileView({super.key});
+  const ProfileView({Key? key}) : super(key: key);
 
   @override
   State<ProfileView> createState() => _ProfileViewState();
 }
 
 class _ProfileViewState extends State<ProfileView> {
-  final ImagePicker picker = ImagePicker();
-  XFile? image;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final ImagePicker _picker = ImagePicker();
 
-  TextEditingController txtName = TextEditingController();
-  TextEditingController txtEmail = TextEditingController();
-  TextEditingController txtMobile = TextEditingController();
-  TextEditingController txtAddress = TextEditingController();
-  TextEditingController txtPassword = TextEditingController();
-  TextEditingController txtConfirmPassword = TextEditingController();
+  late User _user;
+  XFile? _image;
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _mobileController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _user = _auth.currentUser!;
+    _loadUserData();
+  }
+
+  void _loadUserData() async {
+    DocumentSnapshot<Map<String, dynamic>> userData =
+        await _firestore.collection('users').doc(_user.uid).get();
+    setState(() {
+      _nameController.text = userData.data()!['name'];
+      _mobileController.text = userData.data()!['mobile'];
+      _addressController.text = userData.data()!['address'];
+    });
+  }
+
+  Future<void> _updateUserData() async {
+    await _firestore.collection('users').doc(_user.uid).update({
+      'name': _nameController.text,
+      'mobile': _mobileController.text,
+      'address': _addressController.text,
+    });
+  }
+
+  Future<void> _updateProfilePicture() async {
+    XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _image = image;
+      });
+      // Upload image to Firebase Storage and update user data with image URL
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          const SizedBox(
-            height: 46,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Profile",
-                  style: TextStyle(
-                      color: TColor.primaryText,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w800),
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Container()));
-                  },
-                  icon: Image.asset(
-                    "assets/img/more_notification.png",
-                    width: 25,
-                    height: 25,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              color: TColor.placeholder,
-              borderRadius: BorderRadius.circular(50),
-            ),
-            alignment: Alignment.center,
-            child: image != null
-                ? ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Image.file(File(image!.path),
-                        width: 100, height: 100, fit: BoxFit.cover),
-                  )
-                : Icon(
-                    Icons.person,
-                    size: 65,
-                    color: TColor.secondaryText,
-                  ),
-          ),
-          TextButton.icon(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Color(0xffFC6011),
+        actions: [
+          IconButton(
             onPressed: () async {
-              image = await picker.pickImage(source: ImageSource.gallery);
-              setState(() {});
+              await _auth.signOut();
+              // Navigate to login screen or any other screen
             },
-            icon: Icon(
-              Icons.edit,
-              color: TColor.primary,
-              size: 12,
-            ),
-            label: Text(
-              "Edit Profile",
-              style: TextStyle(color: TColor.primary, fontSize: 12),
-            ),
+            icon: Icon(Icons.logout),
           ),
-          Text(
-            "Hi there Emilia!",
-            style: TextStyle(
-                color: TColor.primaryText,
-                fontSize: 16,
-                fontWeight: FontWeight.w700),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              "Sign Out",
-              style: TextStyle(
-                  color: TColor.secondaryText,
-                  fontSize: 11,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-            child: RoundTitleTextfield(
-              title: "Name",
-              hintText: "Enter Name",
-              controller: txtName,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-            child: RoundTitleTextfield(
-              title: "Email",
-              hintText: "Enter Email",
-              keyboardType: TextInputType.emailAddress,
-              controller: txtEmail,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-            child: RoundTitleTextfield(
-              title: "Mobile No",
-              hintText: "Enter Mobile No",
-              controller: txtMobile,
-              keyboardType: TextInputType.phone,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-            child: RoundTitleTextfield(
-              title: "Address",
-              hintText: "Enter Address",
-              controller: txtAddress,
-            ),
-          ),
-          // Padding(
-          //   padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-          //   child: RoundTitleTextfield(
-          //     title: "Password",
-          //     hintText: "* * * * * *",
-          //     obscureText: true,
-          //     controller: txtPassword,
-          //   ),
-          // ),
-          // Padding(
-          //   padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-          //   child: RoundTitleTextfield(
-          //     title: "Confirm Password",
-          //     hintText: "* * * * * *",
-          //     obscureText: true,
-          //     controller: txtConfirmPassword,
-          //   ),
-          // ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: RoundButton(title: "Save", onPressed: () {}),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-        ]),
+        ],
       ),
-    ));
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            GestureDetector(
+              onTap: _updateProfilePicture,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: _image != null ? FileImage(File(_image!.path)) : null,
+                child: _image == null ? Icon(Icons.person, size: 50) : null,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: _user.email != null
+                  ? TextEditingController(text: _user.email)
+                  : TextEditingController(),
+              decoration: InputDecoration(labelText: 'Email'),
+              readOnly: true, // Make email field read-only
+            ),
+            TextField(
+              controller: _mobileController,
+              decoration: InputDecoration(labelText: 'Mobile'),
+            ),
+            TextField(
+              controller: _addressController,
+              decoration: InputDecoration(labelText: 'Address'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () async {
+                await _updateUserData();
+                // Show success message or navigate to another screen
+              },
+              child: Text('Save'),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xffFC6011),
+              )
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
